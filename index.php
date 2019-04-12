7dab0e3f89413e8643fbe1e62ffe31682f44828a
unknown <axesketch@gmail.com>
2019-04-12 23:13:33 +0300

    commit

--- index.php
+++ index.php
@@ -13,22 +13,84 @@
 
 <body>
   <?php require 'process.php'; ?>
-  <div class="row justify-content-center">
-    <form action="" method="POST">
-      <div class="form-group">
-        <label for="">Name</label>
-        <input type="type" name="name" class="form-control" placeholder="Enter your name">
-      </div>
-      <div class="form-group">
-        <label for="">Location</label>
-        <input type="text" name="location" class="form-control" placeholder="Enter your location">
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-primary" name="save">Save</button>
-      </div>
-    </form>
 
-  </div>
+  <?php
+
+  if (isset($_SESSION['message'])) : ?>
+    <div class="alert alert-<?= $_SESSION['msg_type'] ?>"></div>
+
+    <?php
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+    ?>
+    </div>
+  <?php endif ?>
+  <div class="container">
+    <?php
+    $mysqli = new mysqli('localhost', 'root', '', 'crud_location') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM data") or die($mysqli->eror);
+
+    //pre_r($result);
+    ?>
+    <div class="row justify-content-center">
+      <div class="container">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Location</th>
+              <th colspan="2">Action</th>
+            </tr>
+          </thead>
+          <?php
+          while ($row = $result->fetch_assoc()) : ?>
+            <tr>
+              <td><?php echo $row['name'] ?></td>
+              <td><?php echo $row['location'] ?></td>
+              <td>
+                <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+                <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        </table>
+      </div>
+      <?php
+      pre_r($result->fetch_assoc());
+      function pre_r($array)
+      {
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+      }
+      ?>
+
+      <div class="row justify-content-center">
+        <form action="" method="POST">
+       
+          <div class="form-group">
+          <input type="hidden" name="id" value="<?php echo $id; ?>";
+            <label for="">Name</label>
+            <input type="type" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter your name">
+          </div>
+          <div class="form-group">
+            <label for="">Location</label>
+            <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Enter your location">
+          </div>
+          <div class="form-group">
+            <?php
+            if ($update == true) :
+              ?>
+              <button type="submit" class="btn btn-info" name="update">Update</button>
+            <?php else : ?>
+
+              <button type="submit" class="btn btn-primary" name="save">Save</button>
+            <?php endif; ?>
+          </div>
+        </form>
+
+      </div>
+    </div>
 </body>
 
 </html>
